87d75ae22d6237aebb9861e6cb7bf2365519f641
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-02-10 18:57:54 +0100

    updated srtuct SportData

--- Sport/Main/SportData.swift
+++ Sport/Main/SportData.swift
@@ -25,5 +25,44 @@ struct SportModel: Decodable {
 struct GeneralSportModel {
     let sportID: Int
     let sportName: String
-    let sportImage: UIImage
+    let sportType: Sport
+    
+    var image: UIImage? {
+        switch sportType {
+        case .ncaaFootball:
+            return UIImage(named: "ncaaFootball")
+        case .nfl:
+            return UIImage(named: "NFL")
+        case .mlb:
+            return UIImage(named: "MLB")
+        case .nba:
+            return UIImage(named: "NBA")
+        case .ncaaMensBasket:
+            return UIImage(named: "NCAAMensBask")
+        case .nhl:
+            return UIImage(named: "NHL")
+        case .ufsMma:
+            return UIImage(named: "UFC")
+        case .wnba:
+            return UIImage(named: "WNBA")
+        case .mls:
+            return UIImage(named: "MLS")
+        case .epl:
+            return UIImage(named: "EPL")
+        case .fra1:
+            return UIImage(named: "FRA1")
+        case .ger1:
+            return UIImage(named: "GER1")
+        case .esp1:
+            return UIImage(named: "ESP1")
+        case .ita1:
+            return UIImage(named: "ITA1")
+        case .uefaChamp:
+            return UIImage(named: "UEFA")
+        case .fifa:
+            return UIImage(named: "FIFA")
+        case .gpn1:
+            return UIImage(named: "")
+        }
+    }
 }
